a37308c3cf0e09f958b95ad5fde0518aecc5c775
LiWeiHan <weihanli@qq.com>
2017-04-23 15:38:47 +0800

    first commit

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
-
-	"database": "mongodb://root:abc123@ds045031.mongolab.com:45031/userstory",
+	"database": "mongodb://root:abc123@ds161580.mlab.com:61580/user_story",
 	"port": process.env.PORT || 3000,
 	"secretKey": "YourSecretKey"
-
 }
